ddac709d1e2aec5773f6721051c6be5f6a8f0df0
Fra-Vaz92 <francovazza92@gmail.com>
2024-06-23 16:16:51 +0100

    loop and conditions set up
    create a loop and conditions to show a specific sentence near pokemon that are bigger than a specific treshold.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,45 @@
-let pokemonlist = [{name: "Bulbasaur", height: 7, types:["grass", "poison"]}, {name: "Charmander", height: 6, types:"fire"}, {name: "Gastly", height: 1.3, types:["ghost", "poison"]}];
+let pokemonlist = [
+    
+    {
+     name: "Bulbasaur",
+     height: 0.7,
+     types:["grass", "poison"]
+
+    },
+
+    {
+    name: "Charmander",
+    height: 0.6,
+    types:"fire", 
+
+    },
+    
+    {
+    name: "Gastly",
+    height: 1.3,
+    types:["ghost", "poison"]
+
+    },
+
+    {
+     name: "Squirtle",
+     height: 0.5,
+     types:["water"]
+    
+    },
+
+    {
+     name: "Charizard",
+    height: 1.7,
+    types:["fire", "flying"]
+        
+    },
+];
+
+for (let i=0; i < pokemonlist.length; i++) {
+    if (pokemonlist[i].height >1) {
+        document.write("<h3>" + pokemonlist[i].name + " " + "(height:" + " " + pokemonlist[i].height + ")" + "</h3>" + "<p>Wow! it\'s big!</p>");
+    }else {
+        document.write("<h3>" + pokemonlist[i].name + " " + "(height:" + " " + pokemonlist[i].height + ")" + "</h3>");
+    }
+ }
