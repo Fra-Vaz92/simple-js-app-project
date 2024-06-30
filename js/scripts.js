76ab409bba296236e8620ec1b3e9ad84c2d8a666
Fra-Vaz92 <francovazza92@gmail.com>
2024-06-30 21:01:33 +0100

    IIFE function in scrpit.js
    Created a IIFE function in the script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-let pokemonlist = [
+let pokemonRepository = (function () {
+    let pokemonlist = [
     
     {
      name: "Bulbasaur",
@@ -34,12 +35,56 @@ let pokemonlist = [
     types:["fire", "flying"]
         
     },
-];
+]
 
-for (let i=0; i < pokemonlist.length; i++) {
-    if (pokemonlist[i].height >1) {
-        document.write("<h3>" + pokemonlist[i].name + " " + "(height:" + " " + pokemonlist[i].height + ")" + "</h3>" + "<p>Wow! it\'s big!</p>");
-    }else {
-        document.write("<h3>" + pokemonlist[i].name + " " + "(height:" + " " + pokemonlist[i].height + ")" + "</h3>");
+let expectedKeys = ["name", "height", "type"];
+
+    return{
+        add: function(pokemon) {
+            pokemonlist.push(pokemon);
+        },
+
+    getAll: function() {
+        return pokemonlist;
+    },
+
+    add: function (pokemon) {
+        // Check if the parameter is an object
+        if (typeof pokemon === 'object') {
+          // Check if all expected keys are present using Object.keys
+          const pokemonKeys = Object.keys(pokemon);
+          if (pokemonKeys.length === expectedKeys.length && pokemonKeys.every(key => expectedKeys.includes(key))) {
+            // All expected keys are present, add the Pokemon
+            pokemonlist.push(pokemon);
+          } else {
+            console.error("Error: Invalid Pokemon object. Missing required properties.");
+          }
+        } else {
+          console.error("Error: Invalid data type. Only Pokemon objects can be added.");
+        }
+      }
+
     }
- }
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:'Pikachu', height:0.4, type: ['Electric']});
+console.log(pokemonRepository.getAll());
+
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    if (pokemon.height >1) {
+        document.write("<h3>" + pokemon.name + " " + "(height:" + " " + pokemon.height + ")" + "</h3>" + "<p>Wow! it\'s big!</p>");
+    }else {
+        document.write("<h3>" + pokemon.name + " " + "(height:" + " " + pokemon.height + ")" + "</h3>");
+    }
+    console.log(pokemon);
+ });
+
+ function filterbyName(PokemonName){
+    return pokemonRepository
+    .getAll()
+    .filter((name) => name.name.toLowerCase().includes(pokemoName.toLoweCase())
+);
+}
